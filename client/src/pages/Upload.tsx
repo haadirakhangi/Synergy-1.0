1c59488848a888018775020f8dee25f394f34b20
Hatim <hatmsb11@gmail.com>
2024-02-25 01:59:58 +0530

    backend

--- client/src/pages/Upload.tsx
+++ client/src/pages/Upload.tsx
@@ -10,53 +10,88 @@ import {
   CardContent,
   CardMedia,
   TextField,
+  MenuItem,
 } from '@mui/material';
 import axios from 'axios';
 import { Document, Page, pdfjs } from 'react-pdf';
 
 const Upload: React.FC = () => {
-  const [file, setFile] = useState<File | null>(null);
-  const [projectName, setProjectName] = useState<string>('');
+  const [files, setFiles] = useState<File[]>([]);
   const [loading, setLoading] = useState(false);
   const [response, setResponse] = useState<string | null>(null);
-  const [filePreview, setFilePreview] = useState<string | ArrayBuffer | null>(null);
+  const [filePreviews, setFilePreviews] = useState<(string | ArrayBuffer | null)[]>([]);
+  const [selectedFiles, setSelectedFiles] = useState<FileList | null>(null);
+  const [projectName, setProjectName] = useState<string>('');
+  const [editedDetails, setEditedDetails] = useState<{ category: string, document_type: string, index_position: string }>({
+    category: '',
+    document_type: '',
+    index_position: '',
+  });
+  // const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+  //   const selectedFiles = event.target.files;
+  //   if (selectedFiles) {
+  //     const fileList = Array.from(selectedFiles);
+  //     setFiles(fileList);
 
-  const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    const selectedFile = event.target.files && event.target.files[0];
-    if (selectedFile) {
-      setFile(selectedFile);
+  //     // Preview the selected files
+  //     const previews = fileList.map((file) => {
+  //       const reader = new FileReader();
+  //       reader.readAsDataURL(file);
+  //       return reader.result;
+  //     });
 
-      // Preview the selected file
-      const reader = new FileReader();
-      reader.onloadend = () => {
-        setFilePreview(reader.result);
-      };
-      reader.readAsDataURL(selectedFile);
-    }
-  };
+  //     setFilePreviews(previews);
+  //   }
+  // };
 
   const handleProjectNameChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     setProjectName(event.target.value);
   };
 
-  const handleUpload = async () => {
-    if (!file || !projectName) {
-      console.error('Please select a file and enter a project name');
-      return;
+  const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    if (event.target.files) {
+      setSelectedFiles(event.target.files);
     }
+  };
 
-    const formData = new FormData();
-    formData.append('file', file);
-    formData.append('projectName', projectName);
+  const handleUpload = async () => {
+    if (selectedFiles) {
+      const formData = new FormData();
+      for (let i = 0; i < selectedFiles.length; i++) {
+        formData.append('files', selectedFiles[i]);
+      }
+      formData.append('projectName', projectName);
 
+      try {
+        setLoading(true);
+        const response = await axios.post('api/get-category', formData, {
+          headers: {
+            'Content-Type': 'multipart/form-data',
+          },
+        });
+        setResponse(response.data.info)
+        setEditedDetails(response.data.info)
+        console.log("Data received", response.data.info)
+      } catch (error) {
+        console.error('Error uploading files:', error);
+      } finally {
+        setLoading(false); // Stop loading
+      }
+    }
+  };
+
+  const handleDetailsChange = (field: keyof typeof editedDetails, value: string) => {
+    setEditedDetails(prevDetails => ({ ...prevDetails, [field]: value }));
+  };
+
+  const handleConfirmDetails = async () => {
     try {
-      setLoading(true);
-      const response = await axios.post('your-flask-server-endpoint', formData);
-      setResponse(response.data);
+      const confirmDetailsResponse = await axios.post('api/confirm-details', editedDetails);
+      console.log('Confirmed Details:', confirmDetailsResponse.data);
+
+      // You can update the state or perform additional actions based on the confirmation response
     } catch (error) {
-      console.error('Error uploading file:', error);
-    } finally {
-      setLoading(false);
+      console.error('Error confirming details:', error);
     }
   };
 
@@ -74,7 +109,7 @@ const Upload: React.FC = () => {
       >
         <Box sx={{ mt: 1 }}>
           <Typography component="h1" variant="h3" style={{ marginBottom: '1em' }}>
-            Upload Your Document Below
+            Upload Your Documents Below
           </Typography>
           <TextField
             label="Project Name"
@@ -84,7 +119,7 @@ const Upload: React.FC = () => {
             onChange={handleProjectNameChange}
             style={{ marginBottom: '1em' }}
           />
-          <input type="file" onChange={handleFileChange} style={{ width: '100%', marginBottom: '1em' }} />
+          <input type="file" onChange={handleFileChange} multiple style={{ width: '100%', marginBottom: '1em' }} />
           <Button
             variant="contained"
             color="primary"
@@ -102,29 +137,70 @@ const Upload: React.FC = () => {
             Upload
           </Button>
 
-          {loading && <CircularProgress style={{ marginTop: '1em' }} />}
-
-          {filePreview && (
-            <Card style={{ marginTop: '1em', maxWidth: 400 }}>
-              <CardMedia
-                component="img"
-                alt="Uploaded File"
-                height="140"
-                image={URL.createObjectURL(file as Blob)}
-              />
-              <CardContent>
-                <Typography variant="h6">Uploaded File Preview</Typography>
-              </CardContent>
-            </Card>
-          )}
+          {loading && <CircularProgress sx={{
+            color: 'black',  // Set the color to black
+          }} style={{ marginTop: '1em', marginLeft: '50px' }} />}
 
           {response && (
             <div style={{ marginTop: '1em' }}>
-              <Typography component="h1" variant="h3" style={{ marginBottom: '1em' }}>
-                Information Extracted
+              <Typography component="h1" variant="h3" mt={10}>
+                Information Extracted From Document
               </Typography>
-              <Typography variant="h6">Response from server:</Typography>
-              <pre>{JSON.stringify(response, null, 2)}</pre>
+              <Typography component="h1" variant="h6" mt={1} style={{ marginBottom: '1em' }}>
+                Please confirm the following details as it will affect the folder structure :- Project-Name/Category/Dcoument-type
+              </Typography>
+              {/* Input fields for editing details */}
+              <TextField
+                label="Category"
+                variant="outlined"
+                fullWidth
+                value={editedDetails.category}
+                onChange={(e) => handleDetailsChange('category', e.target.value)}
+                style={{ marginBottom: '1em' }}
+              />
+              <TextField
+                select
+                label="Document Type"
+                variant="outlined"
+                fullWidth
+                value={editedDetails.document_type}
+                onChange={(e) => handleDetailsChange('document_type', e.target.value)}
+                style={{ marginBottom: '1em' }}
+              >
+                <MenuItem value="construction_plan">Construction Plan</MenuItem>
+                <MenuItem value="other_docs">Other Documents</MenuItem>
+              </TextField>
+
+              <TextField
+                select
+                label="Index Position"
+                variant="outlined"
+                fullWidth
+                value={editedDetails.index_position}
+                onChange={(e) => handleDetailsChange('index_position', e.target.value)}
+                style={{ marginBottom: '1em' }}
+              >
+                <MenuItem value="right">Right</MenuItem>
+                <MenuItem value="bottom">Bottom</MenuItem>
+                {/* Add other options as needed */}
+              </TextField>
+
+              {/* Confirm details button */}
+              <Button
+                variant="contained"
+                color="primary"
+                onClick={handleConfirmDetails}
+                style={{ width: '100%', marginTop: '1em' }}
+                sx={{
+                  backgroundColor: 'black',
+                  color: 'white',
+                  '&:hover': {
+                    backgroundColor: 'darkslategray',
+                  },
+                }}
+              >
+                Confirm Details
+              </Button>
             </div>
           )}
         </Box>
--- server/main.py
+++ server/main.py
@@ -1,89 +1,113 @@
-import streamlit as st
 from openai import OpenAI
 from dotenv import load_dotenv, find_dotenv
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.output_parsers import StrOutputParser
 from langchain_openai import ChatOpenAI
 from flask import Flask,session,request,jsonify
-from langchain_community.llms.gpt4all import GPT4All
+from pdf2image import convert_from_path
+import google.generativeai as genai
+import cv2
 import os
+import re
+import json
+from PIL import Image
 
 load_dotenv(find_dotenv())
-
-
 app = Flask(__name__)
+app.secret_key = os.environ['SECRET_KEY']
+GOOGLE_API_KEY = os.environ['GOOGLE_API_KEY']
+genai.configure(api_key = GOOGLE_API_KEY)
 
-OPEN_SOURCED_MODEL_PATH_HATIM = 'E:/DJ SANGHVI- AIML DEGREE/DEGREE/HACKATHON/PROJECTS/Techgium/mistral-7b-instruct-v0.1.Q4_0.gguf'
-OPEN_SOURCED_LLM = GPT4All(model= OPEN_SOURCED_MODEL_PATH_HATIM)
-OPENAI_MODEL = 'gpt-3.5-turbo-1106'
-TEMPERATURE =  0.0
-output_parser = StrOutputParser()
-OPENAI_LLM = ChatOpenAI(model= OPENAI_MODEL, temperature=TEMPERATURE)
-
-os.environ['LANGCHAIN_TRACING_V2']= "true"
-os.environ['LANGCHAIN_ENDPOINT']= "https://api.smith.langchain.com"
-os.environ['LANGCHAIN_API_KEY']= "ls__c7b403388bb9404bb123708872b16af6"
-os.environ['LANGCHAIN_PROJECT']= "learning-langsmith"
-
-@app.route('/get-optimizeprompt',methods=['POST'])
-def get_optimizeprompt():
-    try:
-        data = request.get_json()
-        initial_prompt = data.get('initialPrompt', '')
-        print("Initial Prompt:- ", initial_prompt)
-        initial_prompt_template = """You will be given a prompt and your task is to optimize the prompt by understanding the user's required output. Please provide an optimized prompt for the following prompt. \
-        Prompt = {prompt}"""
-        prompt_template = ChatPromptTemplate.from_template(initial_prompt_template)
-        chain = prompt_template | OPENAI_LLM | output_parser
-        optimized_prompt = chain.invoke({"prompt": initial_prompt})
-        print("Optimied prompt:- ",optimized_prompt)
-        return jsonify({'optimized_prompt': optimized_prompt})
+# Bottom crop funtion
+def crop_bottom(input_image_path):
+    image = cv2.imread(input_image_path)
     
-    except Exception as e:
-        return jsonify({'error': str(e)}), 500
+    height, width = image.shape[:2]
+    crop_height = int(height * 0.15)
+    crop_region = image[height - crop_height:height]
+    pil_image = Image.fromarray(cv2.cvtColor(crop_region, cv2.COLOR_BGR2RGB))
+    return pil_image
 
-@app.route('/generate-output',methods=['POST'])
-def get_output():
-    try:
-        data = request.get_json()
-        optimized_prompt = data.get('optimizedPrompt', '')
-        print("Optimized Prompt", optimized_prompt)
-        output_prompt = ChatPromptTemplate.from_template("{input}")
-        output_chain = output_prompt | OPEN_SOURCED_LLM | output_parser
-        output = output_chain.invoke({"input": optimized_prompt})
-        print("Output:", output)
-        return jsonify({'output': output})
+#side crop function
+def crop_right(input_image_path):
+    image = cv2.imread(input_image_path)
     
-    except Exception as e:
-        # Handle errors
-        return jsonify({'error': str(e)}), 500
+    height, width = image.shape[:2]
+    crop_width = int(width * 0.15)
+    crop_region = image[0:height, width - crop_width:width]
+    pil_image = Image.fromarray(cv2.cvtColor(crop_region, cv2.COLOR_BGR2RGB))
+    return pil_image
 
-@app.route('/submit-feedback',methods=['POST'])
-def submit_feedeback():
-    try:
-        data = request.get_json()
-        feedback = data.get('feedback', '')
-        optimized_prompt = data.get('optimizedPrompt', '')
-        print("Optimized Prompt", optimized_prompt)
-        print("Feedback", feedback)
-        feedback_prompt = """You will be given an older prompt given to a Language Model, the output from the language model and feedback\
-        from a user. Optimize the older prompt to generate an output based on the feedback.
-    
-        Old Prompt: {old_prompt}
-        Feedback: {feedback}\n\n\n
-    
-        Construct a better and complete prompt considering the above feedback"""
-        feedback_template = ChatPromptTemplate.from_template(feedback_prompt)
-        feedback_chain = feedback_template | OPENAI_LLM | output_parser
-        prompt_after_feedback = feedback_chain.invoke({"old_prompt": optimized_prompt, "feedback": feedback}) 
-        print("prompt_after_feedback:- ", prompt_after_feedback)
-        return jsonify({'prompt_after_feedback': prompt_after_feedback})
-    
-    except Exception as e:
-        # Handle errors
-        return jsonify({'error': str(e)}), 500
+def get_dict_from_json(response):
+    pattern = re.compile(r'{.*}', re.DOTALL)
+    match = pattern.search(response.text)
+    if match:
+        matched_json = match.group()
+        cleaned_json = matched_json.replace('\\n', '').replace('\n', '')
+        try:
+            data_dict = json.loads(cleaned_json)
+            print(data_dict)
+            return data_dict
+        except json.JSONDecodeError as e:
+            print(f"Error decoding JSON: {e}")
+    else:
+        print("No valid JSON found in the input string.")
 
+@app.route('/get-category', methods=['POST'])
+def get_category():
+    project_name = request.form['projectName']
+    project_name = project_name.replace(" ", "-")
+    prompt = "Analyze the following image and provide a json output that defines which category the image falls in. Also classify the image as either a construction plan or other document. The keys of the json response should be 'category' with the corresponding output being the category the project title and the second key should be 'document_type' with one of the classisfications: 'construction_plan' or 'other_document'. The third key should be the 'index_position' with the value being the position where the index of the construction plan is present (bottom or right). Refer to the examples given to determine this position. If it is not a construction plan return None. Analyze the image carefully and give the json response as described."
+    model = genai.GenerativeModel('gemini-pro-vision')
+    construction_image_example = Image.open('final.jpeg')
+    print(f"Project Name: {project_name}")
+    project_directory = os.path.join("uploads", project_name)
+    files = request.files.getlist('files')
+    if not os.path.exists(project_directory):
+        os.makedirs(project_directory)
+        # Access multiple files
+    for file in files:
+        file_path = os.path.join(project_directory, file.filename)
+        image_path = os.path.join(project_directory, 'img.jpeg')
+        session['file_path'] = file_path
+        session['image_path'] = image_path
+        # Save the file in the project-specific directory
+        file.save(file_path)
+        print(f"Saved file: {file_path}")
+        images = convert_from_path(file_path,poppler_path="C:/Program Files (x86)/poppler-24.02.0/Library/bin")
+        if len(images)>1:
+            first_page = images[0]
+            second_page = images[1]
+            images[0].save(image_path, 'JPEG')
+            response = model.generate_content(['The following is an example of a construction plan with the index at the bottom',construction_image_example, prompt, first_page, second_page])
+        else:
+            first_page = images[0]
+            images[0].save(image_path, 'JPEG')
+            response = model.generate_content(['The following is an example of a construction plan.',construction_image_example, prompt, first_page])
+        response.resolve()
+        # data_dict1 = {'category': 'Electrical', 'document_type': 'construction_plan', 'index_position': 'bottom'}
+        data_dict1 = get_dict_from_json(response)
+    return jsonify({'message': 'Files uploaded successfully',"info": data_dict1}), 200
 
+@app.route('/confirm-details', methods=['POST'])
+def confirm_details():
+    data = request.get_json()
+    print("Data",data)
+    image_path = session['image_path']
+    print("file path",image_path)
+    if data['document_type'] == 'construction_plan':            
+        if data['index_position'] == 'right':
+            cropped_image = crop_right(input_image_path=image_path)
+        elif data['index_position'] == 'bottom':
+            cropped_image = crop_bottom(input_image_path=image_path)
+        prompt_two = "Analyze the following image and provide a json output with the following keys:\n'drawing_status' : //The drawing status mentioned in the image\n'drawing_number' : // The drawing number in the image\n'revision' : // The revision of the drawing\n'title' : // The title of the construction image\n'date' : // The date on the right side. Analyze the image carefully and create a json output with the corresponding values as described."
+
+        model = genai.GenerativeModel('gemini-pro-vision')
+        response_two = model.generate_content([prompt_two, cropped_image], stream=True)
+        response_two.resolve()
+        data_dict2 = get_dict_from_json(response_two)
+        print("Output:- ",data_dict2)
+    return jsonify({'message': 'Details confirmed successfully','info': data_dict2})
 
 @app.route('/logout')
 def logout():
