91ed6db6b61e14ec5d558b5d48916cdffff136fe
Hatim <hatmsb11@gmail.com>
2024-02-25 11:17:43 +0530

    final commit

--- client/src/pages/Upload.tsx
+++ client/src/pages/Upload.tsx
@@ -14,6 +14,7 @@ import {
 } from '@mui/material';
 import axios from 'axios';
 import Swal from 'sweetalert2';
+import ChatWidget from '../components/Layout/ChatWidget';
 
 //import { Document, Page, pdfjs } from 'react-pdf';
 
@@ -223,6 +224,7 @@ const Upload: React.FC = () => {
           )}
         </Box>
       </Container>
+      <ChatWidget/>
     </Layout>
   );
 };
